c0cd7332d8328cd84926be95fa9b016f60584126
Milesq <wmilosz88@gmail.com>
2019-08-29 17:50:57 +0200

    Re add postcss-scss

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+    syntax: 'postcss-scss',
     plugins: [
         require('cssnano'),
         require('colorguard'),
         require('stylelint'),
         require('postcss-preset-env')
     ],
-    map: false
+    map: true
 }
 
 if(process.env.NODE_ENV === 'production') {
