630a3a528111d9ee0332089bfb48542b0d4766f7
Milesq <wmilosz88@gmail.com>
2019-09-03 10:38:39 +0200

    Move indicator from declarative style to class

--- src/js/indicator.js
+++ src/js/indicator.js
@@ -1,80 +1,90 @@
 import { routes } from '../config';
 
-const canvas = document.getElementById('indicator');
-/**
- * @type {CanvasRenderingContext2D}
- */
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = '#0095ff';
+function Indicator(canvas = required()) {
+    this.canvas = canvas;
 
-export default async path => {
-    const key = routes.findIndex(
-        ({ name }) =>
-            name ===
-            path
-                .split('.')
-                .slice(0, -1)
-                .join('.')
-    );
+    /**
+     * @type {CanvasRenderingContext2D}
+     */
+    this.ctx = canvas.getContext('2d');
+    this.ctx.fillStyle = '#0095ff';
+    this.lastPos = null;
 
-    await setTo(key * (canvas.width / 3));
-    ctx.fillRect(key * (canvas.width / 3), 0, canvas.width / 3, canvas.height);
-    ctx.fill();
-};
+    this.move = async path => {
+        const canvas = this.canvas;
+        const key = routes.findIndex(
+            ({ name }) =>
+                name ===
+                path
+                    .split('.')
+                    .slice(0, -1)
+                    .join('.')
+        );
 
-let lastPos = null;
+        await this.setTo(key * (canvas.width / 3));
+        this.ctx.fillRect(key * (canvas.width / 3), 0, canvas.width / 3, canvas.height);
+        this.ctx.fill();
+    };
 
-function setTo(to, len = canvas.width / 3) {
-    const acceleration = 0.25;
-    const reverseAcceleration = 0.4;
+    this.setTo = (to, len = canvas.width / 3) => {
+        const acceleration = 0.25;
+        const reverseAcceleration = 0.4;
 
-    return new Promise(resolve => {
-        let speed = 0;
-        let wasLessThen5 = false;
-        let wasLessThen30 = false;
+        return new Promise(resolve => {
+            let speed = 0;
+            let wasLessThen5 = false;
+            let wasLessThen30 = false;
 
-        async function update() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            const ctx = this.ctx;
 
-            if (lastPos === null) {
-                ctx.fillRect(to, 0, len, canvas.height);
+            async function update() {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                if (this.lastPos === null) {
+                    ctx.fillRect(to, 0, len, canvas.height);
+                    ctx.fill();
+
+                    this.lastPos = to;
+                    return;
+                }
+
+                const direction = this.lastPos < to ? 1 : -1;
+
+                // when distance from indicator to target is less than ...
+                if (Math.abs(this.lastPos - to) < 10 && !wasLessThen5) {
+                    wasLessThen5 = true;
+                    this.lastPos = Math.round(this.lastPos);
+                    speed = direction;
+                } else if (Math.abs(this.lastPos - to) < 45 && !wasLessThen30) {
+                    wasLessThen30 = true;
+                    speed -= reverseAcceleration * direction;
+                } else if (Math.abs(this.lastPos - to) < 5) {
+                    this.lastPos = to;
+                } else {
+                    speed += acceleration * direction;
+                }
+
+                if (Math.round(to) === Math.round(this.lastPos)) {
+                    this.lastPos = to;
+                    resolve();
+                    return;
+                }
+
+                this.lastPos += speed;
+
+                ctx.fillRect(this.lastPos, 0, len, canvas.height);
                 ctx.fill();
 
-                lastPos = to;
-                return;
+                setTimeout(() => window.requestAnimationFrame(update.bind(this)), 3);
             }
 
-            const direction = lastPos < to ? 1 : -1;
-            console.log(lastPos, to, speed);
+            window.requestAnimationFrame(update.bind(this));
+        });
+    };
+}
 
-            // when distance from indicator to target is less than ...
-            if (Math.abs(lastPos - to) < 10 && !wasLessThen5) {
-                wasLessThen5 = true;
-                lastPos = Math.round(lastPos);
-                speed = direction;
-            } else if (Math.abs(lastPos - to) < 45 && !wasLessThen30) {
-                wasLessThen30 = true;
-                speed -= reverseAcceleration * direction;
-            } else if (Math.abs(lastPos - to) < 5) {
-                lastPos = to;
-            } else {
-                speed += acceleration * direction;
-            }
+export default Indicator;
 
-            if (Math.round(to) === Math.round(lastPos)) {
-                lastPos = to;
-                resolve();
-                return;
-            }
-
-            lastPos += speed;
-
-            ctx.fillRect(lastPos, 0, len, canvas.height);
-            ctx.fill();
-
-            setTimeout(() => window.requestAnimationFrame(update), 3);
-        }
-
-        window.requestAnimationFrame(update);
-    });
+function required() {
+    throw 'Parameter is required but not passed!';
 }
--- src/js/router.js
+++ src/js/router.js
@@ -2,12 +2,13 @@ import 'normalize.css';
 import 'regenerator-runtime/runtime';
 import Navigo from 'navigo';
 
-import indicatorRefresh from './indicator';
+import Indicator from './indicator';
 import { routes } from '../config';
 
 const router = new Navigo(undefined, true);
 const appShadow = document.querySelector('#app').attachShadow({ mode: 'open' });
 const parser = new DOMParser();
+const indicator = new Indicator(document.getElementById('indicator'));
 
 routes.forEach(({ path, name, handler }) => {
     if (path == undefined) {
@@ -24,7 +25,7 @@ router.notFound(() => router.navigate('/'));
 router.resolve();
 
 async function setContent(page, callback) {
-    indicatorRefresh(page);
+    indicator.move(page);
     page = await fetch(page).then(x => x.text());
     page = parser.parseFromString(page, 'text/html');
 
