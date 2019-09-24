b215ada5702fb3b359c1fe4f58ba498ceed416e0
Milesq <wmilosz88@gmail.com>
2019-09-24 13:53:02 +0200

    Add animations

--- src/js/who.js
+++ src/js/who.js
@@ -1,12 +1,13 @@
+import anime from 'animejs/lib/anime.es.js';
 import { disablePageScroll, enablePageScroll } from 'scroll-lock';
 
 function Lightbox() {
     const curtain = document.createElement('div');
     curtain.classList.add('curtain');
-    curtain.style.display = 'none';
 
     const contentWrapper = document.createElement('div');
     contentWrapper.classList.add('curtain__content-wrapper');
+    contentWrapper.style.display = 'none';
 
     const content = document.createElement('div');
     content.classList.add('curtain__content');
@@ -20,9 +21,17 @@ function Lightbox() {
     curtain.appendChild(contentWrapper);
     document.body.appendChild(curtain);
 
+    anime
+        .timeline({
+            targets: '.curtain__content-wrapper'
+        })
+        .add({
+            translateY: '100vh'
+        });
+
     this.show = () => {
         disablePageScroll();
-        curtain.style.display = 'flex';
+        contentWrapper.style.display = 'block';
 
         content.innerHTML = `
             <div style="background: url(${this.src})" class="curtain__image"></div>
@@ -31,11 +40,52 @@ function Lightbox() {
                 <p>${this.desc}</p>
             </div>
         `;
+
+        curtain.style.pointerEvents = 'auto';
+
+        anime
+            .timeline({
+                easing: 'easeOutQuad'
+            })
+            .add({
+                targets: '.curtain',
+                backgroundColor: 'rgba(0, 0, 0, .6)',
+                duration: 250,
+                easing: 'linear'
+            })
+            .add(
+                {
+                    targets: '.curtain__content-wrapper',
+                    translateY: 0
+                },
+                '-=200'
+            );
     };
 
     this.hide = () => {
         enablePageScroll();
-        curtain.style.display = 'none';
+
+        anime
+            .timeline({
+                easing: 'easeOutQuad'
+            })
+            .add({
+                targets: '.curtain',
+                backgroundColor: 'rgba(0, 0, 0, 0)',
+                duration: 250,
+                easing: 'linear'
+            })
+            .add(
+                {
+                    targets: '.curtain__content-wrapper',
+                    translateY: '100vh',
+                    complete() {
+                        curtain.style.pointerEvents = 'none';
+                        contentWrapper.style.display = 'none';
+                    }
+                },
+                '-=100'
+            );
     };
 }
 
