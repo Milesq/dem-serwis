8ecc15ade774d5ac3504ec4c7abac5dedee2cfdc
Milesq <wmilosz88@gmail.com>
2019-09-24 21:17:35 +0200

    Hide logo on desktop version

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation');
 const menu = document.querySelector('.mobile-menu');
+const logoLink = document.querySelector('.link-logo__wrapper');
+
+function tg(el, className) {
+    el.classList.toggle(className);
+}
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('is-active');
-    menu.classList.toggle('mobile-menu--is-active');
+    tg(hamburger, 'is-active');
+    tg(menu, 'mobile-menu--is-active');
+    tg(logoLink, 'hidden');
+    tg(nav, 'navigation--mobile-active');
 });
